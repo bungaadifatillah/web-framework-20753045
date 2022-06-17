d5a90f8bca44d5669e8cc9f61e082d9e2e15bca3
bungaadifatillah <86480683+bungaadifatillah@users.noreply.github.com>
2022-06-17 17:05:10 +0700

    Update add.php

--- app/Views/add.php
+++ app/Views/add.php
@@ -2,39 +2,191 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Login</title>
+    <title>Form Tambah Data Diri</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <h1>Login</h1>
+                <h1>From Tambah Data</h1>
             </div>
         </div>
 
     <form action="<?php echo site_url('pages/save');?>" method="post">
         <div class="row mt-3 form-floatin">
             <div class="col-lg-3">
-                <label for="">Username</label>
+                <label for="">NPM</label>
             </div>
             <div class="col-lg-6">
-                <input type="text" class="form-control" name="Username" placeholder="Masukkan Username....">
+                <input type="text" class="form-control" name="npm" placeholder="Masukkan NPM....">
             </div>
         </div>
 
         <div class="row mt-3 form-floatin">
             <div class="col-lg-3">
-                <label for="">Password</label>
+                <label for="">Nama</label>
             </div>
             <div class="col-lg-6">
-                <input type="password" class="form-control" name="password" placeholder="Masukkan Password....">
+                <input type="text" class="form-control" name="nama" placeholder="Masukkan Nama....">
+            </div>
+        </div>
+
+        <div class="row mt-3 form-floatin">
+            <div class="col-lg-3">
+                <label for="">Tempat Lahir</label>
+            </div>
+            <div class="col-lg-6">
+                <input type="text" class="form-control" name="tempat_lahir" placeholder="Masukkan Tempat Lahir....">
+            </div>
+        </div>
+
+        <div class="row mt-3 form-floatin">
+            <div class="col-lg-3">
+                <label for="">Tanggal Lahir</label>
+            </div>
+            <div class="col-lg-6">
+                <input type="date" class="form-control" name="tanggal_lahir" placeholder="Masukkan Tanggal Lahir....">
+            </div>
+        </div>
+
+        <div class="row mt-3 form-floatin">
+            <div class="col-lg-3">
+                <label for="">Alamat</label>
+            </div>
+            <div class="col-lg-6">
+                <input type="text" class="form-control" name="alamat" placeholder="Masukkan Alamat....">
+            </div>
+        </div>
+
+        <div class="row mt-3 form-floatin">
+            <div class="col-lg-3">
+                <label for="">No Hp</label>
+            </div>
+            <div class="col-lg-6">
+                <input type="number" class="form-control" name="no_hp" placeholder="Masukkan No Hp....">
+            </div>
+        </div>
+
+        <div class="row mt-3 form-floatin">
+            <div class="col-lg-3">
+                <label for="">Email</label>
+            </div>
+            <div class="col-lg-6">
+                <input type="text" class="form-control" name="email" placeholder="Masukkan Email....">
+            </div>
+        </div>
+
+        <div class="row mt-3 form-floatin">
+            <div class="col-lg-3">
+                <label for="">Jenis Kelamin</label>
+            </div>
+            <div class="col-lg-6">
+                <input type="checkbox" id="laki" name="jenis_kelamin" value="Laki-laki"> Laki-laki
+                <input type="checkbox" id="perempuan" name="jenis_kelamin" value="Perempuan"> Perempuan
+            </div>
+        </div>
+
+        <div class="row mt-3 form-floatin">
+            <div class="col-lg-3">
+                <label for="">Agama</label>
+            </div>
+            <div class="col-lg-6">
+                <select id="agama" name="agama">
+                    <option value="Islam">Islam</option>
+                    <option value="Kristen">Kristen</option>
+                    <option value="Hindu">Hindu</option>
+                    <option value="Buddha">Buddha</option>
+                    <option value="Konghucu">Konghucu</option>
+                    <option value="Katolik">Katolik</option>
+                </select>
+            </div>
+        </div>
+
+        <div class="row mt-3 form-floatin">
+            <div class="col-lg-3">
+                <label for="">SD</label>
+            </div>
+            <div class="col-lg-6">
+                <input type="text" class="form-control" name="sd" placeholder="Masukkan Riwayat Pendidikan....">
+            </div>
+        </div>
+
+        <div class="row mt-3 form-floatin">
+            <div class="col-lg-3">
+                <label for="">SMP</label>
+            </div>
+            <div class="col-lg-6">
+                <input type="text" class="form-control" name="smp" placeholder="Masukkan Riwayat Pendidikan....">
+            </div>
+        </div>
+
+        <div class="row mt-3 form-floatin">
+            <div class="col-lg-3">
+                <label for="">SMA</label>
+            </div>
+            <div class="col-lg-6">
+                <input type="text" class="form-control" name="sma" placeholder="Masukkan Riwayat Pendidikan....">
+            </div>
+        </div>
+
+        <div class="row mt-3 form-floatin">
+            <div class="col-lg-3">
+                <label for="">Skill</label>
+            </div>
+            <div class="col-lg-6">
+                <input type="text" class="form-control" name="skill" placeholder="Masukkan Skill....">
+            </div>
+        </div>
+
+        <div class="row mt-3 form-floatin">
+            <div class="col-lg-3">
+                <label for="">Skill2</label>
+            </div>
+            <div class="col-lg-6">
+                <input type="text" class="form-control" name="skill2" placeholder="Masukkan Skill....">
+            </div>
+        </div>
+
+        <div class="row mt-3 form-floatin">
+            <div class="col-lg-3">
+                <label for="">Skill3</label>
+            </div>
+            <div class="col-lg-6">
+                <input type="text" class="form-control" name="skill3" placeholder="Masukkan Skill....">
+            </div>
+        </div>
+
+        <div class="row mt-3 form-floatin">
+            <div class="col-lg-3">
+                <label for="">Pengalaman</label>
+            </div>
+            <div class="col-lg-6">
+                <input type="text" class="form-control" name="pengalaman" placeholder="Masukkan Pengalaman Kerja....">
+            </div>
+        </div>
+
+        <div class="row mt-3 form-floatin">
+            <div class="col-lg-3">
+                <label for="">Pengalaman2</label>
+            </div>
+            <div class="col-lg-6">
+                <input type="text" class="form-control" name="pengalaman2" placeholder="Masukkan Pengalaman Kerja....">
+            </div>
+        </div>
+
+        <div class="row mt-3 form-floatin">
+            <div class="col-lg-3">
+                <label for="">Pengalaman3</label>
+            </div>
+            <div class="col-lg-6">
+                <input type="text" class="form-control" name="pengalaman3" placeholder="Masukkan Pengalaman Kerja....">
             </div>
         </div>
 
         <div>
             <div>
-                <button class="btn btn-primary" type="submit">Login</button>
+                <button class="btn btn-primary" type="submit">Simpan</button>
             </div>
         </div>
     </form>
